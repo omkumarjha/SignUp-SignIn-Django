92d3f5700d529c26fc7978fada149a62b840e9e4
om <omkumar1892002@gmail.com>
2023-11-11 18:04:06 +0530

    Have written the code for validation in form using django

--- myproject/verify/forms.py
+++ myproject/verify/forms.py
@@ -6,7 +6,38 @@ class SignUp(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Your Password"}))
     repassword = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Repeat Your Password"}))
 
+    def clean(self):
+        cleaned_data = super().clean()
+
+        Valname = self.cleaned_data["name"]
+        Valpassword = self.cleaned_data["password"]
+        Valrepassword = self.cleaned_data["repassword"]
+
+        if(Valpassword != Valrepassword):
+            print("Yes password")
+            raise forms.ValidationError("Password and Repassword should be same")
+
+        if(not Valname[0].isalpha()):
+            print("No password")
+            raise forms.ValidationError("Name should start with alphabets ")
+
+        if(len(Valpassword) < 10 and len(Valrepassword) < 10):
+            print("No yes password")
+            raise forms.ValidationError("Length of password should be greater then 10.")
+
+        return cleaned_data
+
 
 class SignIn(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Your Name"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Your Password"}))
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+
+        if(not name[0].isalpha()):
+            # print("No password") 
+            raise forms.ValidationError("Name should start with alphabets ")
+
+        return name
+
--- myproject/verify/views.py
+++ myproject/verify/views.py
@@ -7,10 +7,36 @@ def home(request):
     return render(request,'verify/home.html')
 
 def signup(request):
-    fm = SignUp()
+    if request.method == "POST":
+        fm = SignUp(request.POST)
+
+        if fm.is_valid():
+            print("Form is validated successfully ")
+            print("Name : ",fm.cleaned_data["name"])
+            print("email : ",fm.cleaned_data["email"])
+            print("password : ",fm.cleaned_data["password"])
+            print("repassword : ",fm.cleaned_data["repassword"])
+            fm = SignUp()
+        else:
+            print("Error is comming....")
+
+    else:
+        fm = SignUp()
     return render(request,'verify/signup.html',{"forms":fm})
     
 def signin(request):
-    fm = SignIn()
+    if request.method == "POST":
+        fm = SignIn(request.POST)
+
+        if fm.is_valid():
+            print("Form is validated successfully ")
+            print("Name : ",fm.cleaned_data["name"])
+            print("password : ",fm.cleaned_data["password"])
+            fm = SignIn()
+        else:
+            print("Error is comming....")
+
+    else:
+        fm = SignIn()
     return render(request,'verify/signin.html',{"forms": fm})
 
