bfe5b09bd972fdda4d7620abf557fd002103fe24
om <omkumar1892002@gmail.com>
2023-11-12 13:01:03 +0530

    Added code for signup and signin

--- myproject/verify/views.py
+++ myproject/verify/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import SignUp , SignIn
+from django.contrib.auth.models import User
+from django.contrib import messages 
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
@@ -12,11 +15,20 @@ def signup(request):
 
         if fm.is_valid():
             print("Form is validated successfully ")
-            print("Name : ",fm.cleaned_data["name"])
-            print("email : ",fm.cleaned_data["email"])
-            print("password : ",fm.cleaned_data["password"])
-            print("repassword : ",fm.cleaned_data["repassword"])
-            fm = SignUp()
+
+            name = fm.cleaned_data["name"]
+            email = fm.cleaned_data["email"]
+            password = fm.cleaned_data["password"]
+
+            # We have a module User . jiska humne object create kara hai aur because yeh entries hum database mai save karne wale hai to first apne ko sare current migrations ko migrate karna hoga and then we can create user account and show them ki aapka account crete ho chuka hai abb login kariye 
+
+            myuser = User.objects.create_user(name,email,password)
+            myuser.save()
+
+            messages.success(request,"Your account has been successfully created.")
+
+            return redirect("signin")
+
         else:
             print("Error is comming....")
 
@@ -30,9 +42,22 @@ def signin(request):
 
         if fm.is_valid():
             print("Form is validated successfully ")
-            print("Name : ",fm.cleaned_data["name"])
-            print("password : ",fm.cleaned_data["password"])
-            fm = SignIn()
+
+            name = fm.cleaned_data["name"]
+            password = fm.cleaned_data["password"]
+            
+            # Agar user nae signin page pe credentials sahi diye hai to user object retrn hoga othewise none return hoga
+            user = authenticate(username=name,password=password)
+
+            if user is not None:
+                login(request,user)
+                return render(request,"verify/loggedin.html",{"name":name})
+            else:
+                messages.error(request,"Bad Credentials")
+                print("Bad ")
+                return redirect("signin")
+
+
         else:
             print("Error is comming....")
 
